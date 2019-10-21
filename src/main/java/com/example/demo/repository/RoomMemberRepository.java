6d22e9700192f5997ab11ae67ce68230dccc39f6
hagyeong <gkrud4513@naver.com>
2019-10-21 20:59:09 +0900

    [UPDATE] RoomMemberRepository update

--- src/main/java/com/example/demo/repository/RoomMemberRepository.java
+++ src/main/java/com/example/demo/repository/RoomMemberRepository.java
@@ -3,8 +3,13 @@ package com.example.demo.repository;
 import com.example.demo.entity.RoomMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RoomMemberRepository extends JpaRepository<RoomMember, Integer> {
     Optional<RoomMember> findByUserIdAndRoomId(String userId, Integer roomId);
+
+    Optional<RoomMember> findByUserId(String userId);
+
+    List<RoomMember> findByRoomId(Integer roomId);
 }
