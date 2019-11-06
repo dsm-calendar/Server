d685f45eda025d18f765e1fca3640e4199d72f61
hagyeong <gkrud4513@naver.com>
2019-11-06 16:23:17 +0900

    [ADD] MyPageController and MyPageService

---
+++ src/main/java/com/example/demo/controller/MyPageController.java
@@ -0,0 +1,46 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.*;
+import com.example.demo.service.CertifiedService;
+import com.example.demo.service.MyPageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class MyPageController {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private LoginUserRepository loginUserRepository;
+    @Autowired
+    private AdminRepository adminRepository;
+    @Autowired
+    private CalendarRepository calendarRepository;
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+    @Autowired
+    private RoomRepository roomRepository;
+    @Autowired
+    private RoomMemberRepository roomMemberRepository;
+
+    private MyPageService myPageService;
+    private CertifiedService certifiedService;
+
+    public MyPageController() {
+        this.myPageService = new MyPageService();
+        this.certifiedService = new CertifiedService();
+    }
+
+    @GetMapping(value = "/myPage")
+    public ResponseEntity<Student> myPageRequest(@RequestHeader Integer loginUserId){
+        certifiedService.isLogin(loginUserId,loginUserRepository,userRepository,adminRepository);
+        return ResponseEntity.ok(userRepository.findById(loginUserRepository.findById(loginUserId).get().getUserId()).get());
+    }
+    @PutMapping(value = "/myPage/updateMyIcon")
+    public ResponseEntity<Student> updateMyPageRequest(@RequestHeader Integer loginUserId, @RequestBody Student student){
+        certifiedService.isLogin(loginUserId,loginUserRepository,userRepository,adminRepository);
+        return ResponseEntity.ok(myPageService.updateIndex(student,userRepository,loginUserRepository.findById(loginUserId)));
+    }
+}
---
+++ src/main/java/com/example/demo/service/MyPageService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.LoginUser;
+import com.example.demo.entity.Student;
+import com.example.demo.repository.UserRepository;
+
+import java.util.Optional;
+
+public class MyPageService {
+
+    public Student updateIndex(Student student, UserRepository userRepository, Optional<LoginUser> loginUser) {
+        Student user = userRepository.findById(loginUser.get().getUserId()).get();
+        user.setStudent(student);
+        return userRepository.save(user);
+    }
+}
