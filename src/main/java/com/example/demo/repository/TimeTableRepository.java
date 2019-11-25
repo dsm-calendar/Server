2a6e77c276f8412de3ea219a91483aab0117b8da
hagyeong <gkrud4513@naver.com>
2019-11-25 10:19:26 +0900

    [UPDATE] TimeTableRepository update

--- src/main/java/com/example/demo/repository/TimeTableRepository.java
+++ src/main/java/com/example/demo/repository/TimeTableRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.entity.TimeTable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TimeTableRepository extends JpaRepository<TimeTable,Integer> {
+    List<TimeTable> findByTimeTableIndexBetween(Integer substring, Integer str2);
 }
